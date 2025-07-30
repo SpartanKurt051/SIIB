ac4bd1d2f3e3dbf30caf0caf2a48a6a2eeb35aa8
SpartanKurt051 <37214287+SpartanKurt051@users.noreply.github.com>
2025-07-30 17:37:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("Hello world")
 import pandas as pd
 
-df = pd.read_excel('Case- Role_Engagement_Case.xlsx')
+df = pd.read_excel('subfolder/Case- Role_Engagement_Case.xlsx')
 st.write(df)  # Display the dataframe
 
 st.bar_chart(df)  # Simple bar chart (works if your data is numeric)

bfce61a8901bf791707bfce3880860ab6fd5bbd8
SpartanKurt051 <37214287+SpartanKurt051@users.noreply.github.com>
2025-07-31 16:46:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ import pandas as pd
 employee_count = 100
 
 file_path = 'SIIB.xlsx'
-df = pd.read_excel(file_path)
-print(df)
-# Print the column names
-print("Column names:", df.columns.tolist())
+try:
+    df = pd.read_excel(file_path)
+    print("Column names:", df.columns.tolist())
+except Exception as e:
+    print("Error reading Excel file:", e)
 
 strength_scores = {
     'Customer Centricity': 96,

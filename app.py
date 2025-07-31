ff8f4d03bdc3c534ba48bb2d3840e62dea50eff0
SpartanKurt051 <37214287+SpartanKurt051@users.noreply.github.com>
2025-07-31 16:48:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-import streamlit as st
 import pandas as pd
-# Provided values
-employee_count = 100
 
+# Path to your Excel file
 file_path = 'SIIB.xlsx'
-try:
-    df = pd.read_excel(file_path)
-    print("Column names:", df.columns.tolist())
-except Exception as e:
-    print("Error reading Excel file:", e)
+
+# Read the Excel file
+df = pd.read_excel(file_path)
+
+# Display the entire DataFrame
+print(df)
